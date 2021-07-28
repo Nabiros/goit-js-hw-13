e97501cf5c7037868d723db9ecf61c25c427da3e
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-07-28 20:59:36 +0300

    added styles & fix btn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,78 @@
 import './sass/main.scss';
+import apiService from './js/apiServise';
+import refs from './js/refs';
+import galleryMarkup from './templates/gallary-item.hbs';
+import * as basicLightbox from 'basiclightbox';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
+
+const { form, gallery, loadMoreBtn, loadMoreLabel, loadMoreSpinner } = refs;
+
+const loadMore = {
+  enable() {
+    loadMoreBtn.disabled = false;
+    loadMoreLabel.textContent = 'Показать ещё';
+    loadMoreSpinner.classList.add('is-hidden');
+  },
+  disable() {
+    loadMoreBtn.disabled = true;
+    loadMoreLabel.textContent = 'Загружаем...';
+    loadMoreSpinner.classList.remove('is-hidden');
+  },
+  show() {
+    loadMoreBtn.classList.remove('is-hidden');
+  },
+};
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const input = e.currentTarget.elements.query.value;
+  apiService.query = input.trim();
+
+  if (!apiService.query) return;
+  clearImgGallery();
+
+  apiService.resetPage();
+
+  fetchMoreImg();
+
+  gallery.addEventListener('click', e => {
+    e.preventDefault();
+
+    if (e.target.nodeName === 'IMG') {
+      const instance = basicLightbox.create(`
+        <img src="${e.target.dataset.source}">
+        `);
+      instance.show();
+    }
+  });
+
+  form.reset();
+});
+
+loadMoreBtn.addEventListener('click', fetchMoreImg);
+
+function fetchMoreImg() {
+  loadMore.disable();
+
+  apiService.fetchImg().then(data => {
+    renderImageGallery(data);
+    loadMore.show();
+    loadMore.enable();
+
+      loadMoreBtn.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+  });
+
+}
+
+function renderImageGallery(data) {
+  const markup = galleryMarkup(data);
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearImgGallery() {
+  gallery.innerHTML = '';
+}
--- src/js/apiServise.js
+++ src/js/apiServise.js
@@ -3,3 +3,36 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
 const API_KEY = '22668325-829eebec8d763e484fb933a72';
+
+export default {
+  searchQuery: '',
+  page: 1,
+
+  fetchImg() {
+    return axios
+      .get(
+        `/?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${API_KEY}`,
+      )
+      .then(({ data }) => {
+        this.incrementPage();
+        return data.hits;
+      })
+      .catch(error => console.error(error));
+  },
+
+  incrementPage() {
+    this.page += 1;
+  },
+
+  resetPage() {
+    this.page = 1;
+  },
+
+  get query() {
+    return this.searchQuery;
+  },
+
+  set query(value) {
+    this.searchQuery = value;
+  },
+};
